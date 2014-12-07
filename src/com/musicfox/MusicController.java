fb47cfd70dd183039594e0d2e3f1afdd25f28afb
miglrodri <migl.rodri@gmail.com>
2014-12-07 19:04:03 +0000

    Added to ignore the OWL file. Going to check changes on all files

--- src/com/musicfox/MusicController.java
+++ src/com/musicfox/MusicController.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 // TODO quando é feito um pedido para mostrar artista por id, se esse artista nao tiver a info das stats toda completa a query cai por terra!!
 // solução seria fazer uma BD que populasse todos os dados, mesmo com 0 ou nulls. do lado do sparql teria que se andar a fazer vários pedidos, a cada estatística..
 // no entanto ainda devemos investigar porque acho que dá para declarar campos opcionais na pesquisa sparql, no entanto nao sei como funca e se dá mais trab.
@@ -11,8 +9,6 @@
 package com.musicfox;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
